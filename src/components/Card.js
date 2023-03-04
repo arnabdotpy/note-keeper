23871979c2976381cbd21856555cbe5f369ae9be
Arnab <mr.roy.arnab@gmail.com>
2023-03-04 21:36:27 +0530

    working_nontes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from '@/styles/Card.module.css'
 
-function Card() {
+function Card(props) {
   return (
     <>
     <div className={styles.card}>
-        <h1 className={styles.title}>This is a title</h1>
-        <p className={styles.content}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quisquam soluta adipisci delectus iste dolorum quasi quod sunt, tempore mollitia?</p>
+        <h1 className={styles.title}>{props.title}</h1>
+        <p className={styles.content}>{props.content}</p>
     </div>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Card from '@/components/Card'
 
-const inter = Inter({ subsets: ['latin'] })
+function createNote(ctx) {
+  return (
+    <>
+    <Card 
+    key = {ctx._id}
+    title = {ctx.title}
+    content = {ctx.content}
+    />
+    </>
+  )
+}
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
     {/* Header Section */}
-    <form className={styles.form} action="http://localhost:3000/api/addnote" method="post">
+    <form className={styles.form} action="https://notekeeper.arnab.tech/api/addnote" method="post">
       <textarea className={styles.textarea1} placeholder="Title goes here" name="title"></textarea>
       <textarea className={styles.textarea2} placeholder="Note goes here" name="content"></textarea>
       <button className={styles.btn} >Add Note</button>
     </form>
+    <div className={styles.notes}>
+      {props.notes.map(createNote)}
+    </div>
     </>
-  )
+)}
+
+export async function getServerSideProps(context) {
+  let data = await fetch("https://notekeeper.arnab.tech/api/getnotes");
+  let notes = await data.json();
+return {    
+  props: {notes}, // will be passed to the page component as props
+}
 }
