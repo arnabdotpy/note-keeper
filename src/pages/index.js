388543ca7d564049ae0cfc5e9ca9fbc55efd0753
Arnab <mr.roy.arnab@gmail.com>
2023-03-04 21:05:57 +0530

    Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
-import Card from '@/components/card'
 import styles from '@/styles/Home.module.css'
+import Card from '@/components/Card'
 
 const inter = Inter({ subsets: ['latin'] })
 
